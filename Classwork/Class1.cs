0cf1830f3996178ef0cd98d648bb848948888a8c
andrejmanin <andrejmanin54@gmail.com>
2024-12-21 15:24:31 +0200

    Innit Commit

---
+++ Classwork/Class1.cs
@@ -0,0 +1,62 @@
+﻿namespace Classwork;
+
+public class PracticeTask
+{
+    public delegate bool isPair(int number);
+    public delegate int square(int number);
+    
+    public static bool pair(int number) => number % 2 == 0;
+    public static int numSquare(int number) => number * number;
+
+    public static Func<int, int> cube(int number)
+    {
+        return delegate(int number)
+        {
+            return number * number * number;
+        };
+    }
+    
+    private static bool isLeepYear(int year) => year % 4 == 0 && year % 100 != 0;
+
+    public static Func<int, int, int, bool> itDay(int year, int month, int day)
+    {
+        return delegate(int year, int monthOfYear, int dayOfMonth)
+        {
+            if (monthOfYear != 9)
+            {
+                return false;
+            }
+
+            if (isLeepYear(year))
+            {
+                return dayOfMonth == 13;
+            }
+
+            return dayOfMonth == 12;
+        };
+    }
+
+    public static Func<int[], int> findMaxInArray(int[] array)
+    {
+        return delegate(int[] array)
+        {
+            return array.Max();
+        };
+    }
+    
+    public static Func<int[], int> findMinInArray(int[] array)
+    {
+        return delegate(int[] array)
+        {
+            return array.Min();
+        };
+    }
+
+    public static Func<int[], int[]> oddNumbers(int[] array)
+    {
+        return delegate(int[] array)
+        {
+            return array.Where(n => n % 2 != 0).ToArray();
+        };
+    }
+}
---
+++ lambda-tests/Test1.cs
@@ -0,0 +1,56 @@
+﻿using Classwork;
+
+namespace lambda_tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void pairTest()
+    {
+        PracticeTask.isPair isPair = new PracticeTask.isPair(PracticeTask.pair);
+        const int number = 5;
+        isPair(number);
+    }
+
+    [TestMethod]
+    public void squareTest()
+    {
+        PracticeTask.square square = new PracticeTask.square(PracticeTask.numSquare);
+        const int number = 5;
+        square(number);
+    }
+
+    [TestMethod]
+    public void cubeTest()
+    {
+        PracticeTask.cube(10);
+    }
+
+    [TestMethod]
+    public void itDayTest()
+    {
+        PracticeTask.itDay(2024, 12, 5);
+    }
+
+    [TestMethod]
+    public void findMaxInArrayTest()
+    {
+        int[] array = new int[] { 2, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        PracticeTask.findMaxInArray(array);
+    }
+
+    [TestMethod]
+    public void findMinInArrayTest()
+    {
+        int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        PracticeTask.findMinInArray(array);
+    }
+
+    [TestMethod]
+    public void oddNumberTest()
+    {
+        int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        PracticeTask.oddNumbers(array);
+    }
+}
